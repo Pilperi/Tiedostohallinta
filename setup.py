68d8b7d1ddbe0928a58b32deafab026e668603c7
Pilperi <jouni.dx@gmail.com>
2021-03-29 20:34:06 +0300

    hups

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ setuptools.setup(
     long_description=open('README.md').read(),
     packages=setuptools.find_packages(),
     install_requires=["mutagen", "hashlib"],
-	python_requires=">=3.8, <4"
+	python_requires=[">=3.8, <4"]
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.8',
     ],
     include_package_data=True,
-    package_data={'': ['./kansiovakiot.ini']},
+    package_data={'': ['./kansiovakiot.ini']}
 )
